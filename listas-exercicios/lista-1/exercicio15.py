d5414277ef0f3b65c309c731e8431b6bd976f454
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2026-02-17 16:09:47 -0300

    fix: ajuste na questão

--- listas-exercicios/lista-1/exercicio15.py
+++ listas-exercicios/lista-1/exercicio15.py
@@ -1,12 +1,3 @@
-# Um banco deseja simular o valor final de um empréstimo com juros simples. Dados de entrada:
-# Valor do empréstimo
-# Taxa de juros mensal (%)
-# Quantidade de meses
-# Desenvolva um algoritmo que:
-# Leia os dados;
-# Calcule o valor final a ser pago;
-# Informe o total de juros pagos.
-
 valor_emprestimo = float(input("Valor do empréstimo: "))
 taxa_juros_mensal = float(input("Taxa de juros mensal(%): "))
 qtd_meses = int(input("Quantidade de meses: "))
