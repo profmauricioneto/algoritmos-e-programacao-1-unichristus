4efb7d0431a82ee6ce148805e79bd2b67df6114a
Maurício Moreira Neto <89693991+profmauricioneto@users.noreply.github.com>
2026-04-10 14:22:32 +0000

    add: resolucao da questao de repeticao

--- curso-2026-1/unidade 4/praticas/exercicio06.py
+++ curso-2026-1/unidade 4/praticas/exercicio06.py
@@ -1,3 +1,9 @@
+quantidade_acima_100 = 0
+media_lucro_animacao = 0
+titulo_mais_lucrativo = ''
+maior_lucro = 0
+contador_animacao = 0
+
 while True:
     titulo = input('Digite o título do filme: ')
     print(f'{titulo.upper()}')
@@ -6,6 +12,23 @@ while True:
     apuracao = float(input('Valor da apuração (em milhões): '))
 
     # LOGICA DA APLICACAO ----
+    # Apresenta o lucro e o título de cada filme
+    lucro = apuracao - orcamento
+    print(f'{titulo} possui um lucro de {lucro}')
+
+    # Calculo para saber o maior lucro
+    if (lucro > maior_lucro):
+        maior_lucro = lucro
+        titulo_mais_lucrativo = titulo
+
+    # Calculo da médias dos lucros de animação
+    if (genero == 4):
+        media_lucro_animacao += lucro
+        contador_animacao += 1
+    
+    # Quantidade de filmes com lucro acima de 100M
+    if (lucro > 100):
+        quantidade_acima_100 += 1
 
     opcao = input('Deseja Continuar? [S/N]')
     if (opcao.lower() == 'nao' or opcao.lower() == 'não' or opcao.lower() == 'n'):
@@ -13,3 +36,11 @@ while True:
     else:
         continue
 
+
+media_lucro_animacao = media_lucro_animacao/contador_animacao
+
+print(f'*' * 30)
+print(f'O filme {titulo_mais_lucrativo} é o mais lucrativo com {maior_lucro}')
+print(f'Quantidade de filmes com lucro acima de 100M: {quantidade_acima_100}')
+print(f'Média dos lucros dos filmes de animação: {media_lucro_animacao}')
+print(f'*' * 30)
