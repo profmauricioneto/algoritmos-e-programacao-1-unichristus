c75a25117460623bd027863fda7c0b366ba8cd5e
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2026-08-10 11:20:18 -0300

    add: aula sobre entrada e saida de dados.

---
+++ curso-2026-2/estrutura-sequencial/entrada-dados.py
@@ -0,0 +1,9 @@
+nome = input('Digite o nome do usuário: ')
+# print('Bom dia', nome)
+print(f'Bom dia {nome}')
+
+idade = int(input('Digite a sua idade: '))
+print(type(idade))
+# print(idade + 2)
+
+print(f'Olá {nome}, sua idade é {idade}, você nasceu em: {2026 - idade}')
---
+++ curso-2026-2/estrutura-sequencial/variaveis.py
@@ -0,0 +1,16 @@
+"""
+EXERCICIO DE PYTHON
+Meu primeiro código Python!
+"""
+# criando variáveis e verificando o tipo
+idade = 33
+print(type(idade))
+
+nota = 7.8
+print(type(nota))
+
+nome = 'Maurício'
+print(type(nome))
+
+eh_professor = True
+print(type(eh_professor))
