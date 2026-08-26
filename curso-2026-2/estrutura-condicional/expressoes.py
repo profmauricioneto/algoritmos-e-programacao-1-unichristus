c48bf2603d7218345846674ee1de4d243bcac376
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2026-08-26 18:33:06 -0300

    add: aula de condicionais e expressões lógicas

---
+++ curso-2026-2/estrutura-condicional/expressoes.py
@@ -0,0 +1,29 @@
+A = 5
+B = 10
+C = -8
+D = 1.5
+
+a = 2 * A % 3 - C
+print(f'a = {a}')
+
+b = (30 % 4 * 3 ** 3) * -1
+print(f'b = {b}')
+
+c = ((20 // 3) // 3) + 8**2/2
+print(f'c = {c}')
+
+d = -C**2 + (D * 10)/A
+print(f'd = {d}')
+
+exp1 = 3 * 6 == 36/2
+print(f'resultado = {exp1}')
+
+exp2 = 15 % 4 < 19 % 6
+print(15%4)
+print(19%6)
+print(f'resultado = {exp2}')
+
+exp3 = 3 * 5 // 4 <= 3**2 / 0.5
+print(3 * 5 // 4)
+print(3**2 / 0.5)
+print(f'resultado = {exp3}')
--- curso-2026-2/estrutura-condicional/situacao-aluno.py
+++ curso-2026-2/estrutura-condicional/situacao-aluno.py
@@ -4,17 +4,27 @@ nota2 = float(input('Digite a nota 2: '))
 media = (nota1 + nota2)/2
 print(f'A média = {media: .2f}')
 
-# if (media >= 7.0):
-#     print('Aprovado')
-# elif (media < 4.0):
-#     print('Reprovado!')
-# else:
-#     print('Final')
-
 if (media >= 7.0):
-    print('Aprovado! Vaitimbora!')
+    print('Aprovado')
+elif (media < 4.0):
+    print('Reprovado!')
 else:
-    if(media < 4.0):
-        print('Reprovado! Fica ai!')
+    print('Final')
+    
+    nota_final = float(input('Digite a nota final: '))
+    media_final = (media + nota_final)/2
+    print(f'Media da Final = {media_final: .2f}')
+    
+    if (media_final >= 5.0):
+        print(f'Aprovado na Final')
     else:
-        print('Final! Fica mais um pouco!')
+        print(f'Reprovado na Final')
+
+print('Fim de Algoritmo.')
+# if (media >= 7.0):
+#     print('Aprovado! Vaitimbora!')
+# else:
+#     if(media < 4.0):
+#         print('Reprovado! Fica ai!')
+#     else:
+#         print('Final! Fica mais um pouco!')
